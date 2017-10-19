624be50097cbdea11739d14e4b5c2fc12e351544
Jose Tello V <31993847+jtello2017@users.noreply.github.com>
2017-10-18 23:34:11 -0400

    jeje

---
+++ MedForm/app/src/main/java/cl/tello_urtubia/medform/CrearRecetaActivity.java
@@ -0,0 +1,100 @@
+package cl.tello_urtubia.medform;
+
+import android.content.Intent;
+<<<<<<< HEAD
+import android.database.sqlite.SQLiteDatabase;
+=======
+import android.support.v7.app.ActionBar;
+>>>>>>> 97e382aef9194d9f112648a63d117d9fda8b9142
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import cl.tello_urtubia.medform.Utilidades.Utilidades;
+
+import static cl.tello_urtubia.medform.R.id.campoDireccion;
+import static cl.tello_urtubia.medform.R.id.campoFecha;
+import static cl.tello_urtubia.medform.R.id.campoRut;
+
+public class CrearRecetaActivity extends AppCompatActivity {
+
+    EditText campoMedicamento, campoDiagnostico;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_receta);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_crearReceta);
+        setSupportActionBar(toolbar);
+
+        campoMedicamento = (EditText) findViewById(R.id.campoMedicamento);
+        campoDiagnostico = (EditText) findViewById(R.id.campoDiagnostico); // Falta crear estos Edits View
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_crear_receta, menu);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_user_settings:
+                break;
+            case R.id.action_settings:
+                break;
+            case R.id.action_print:
+                break;
+            case android.R.id.home:
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick (View view) {
+        String nombre = getIntent().getStringExtra("nombre");
+        String rut = getIntent().getStringExtra("rut");
+        String medicamento = campoMedicamento.getText().toString();
+        String diagnostico = campoDiagnostico.getText().toString();
+        registrarRecetasSQL(nombre, rut, medicamento, diagnostico);
+        crearArchivoReceta(nombre,rut,medicamento,diagnostico);
+
+        Intent intent = new Intent(this,MainActivity.class); // Luego de crear la receta, volvemos al inicio
+        startActivity(intent);
+    }
+
+    public void crearArchivoReceta(String nombre, String rut, String medicamento, String diagnostico) {
+    }
+
+    public void registrarRecetasSQL(String nombre, String rut, String medicamento, String diagnostico) {
+
+        RecetaSQLHelper conn = new RecetaSQLHelper(this, "bd_recetas", null, 1);
+
+        SQLiteDatabase db = conn.getWritableDatabase();
+
+        String insert = "INSERT INTO "+ Utilidades.TABLA_RECETA+" ( "+Utilidades.CAMPO_NOMBRE+","
+                +Utilidades.CAMPO_RUT+","+Utilidades.CAMPO_MEDICAMENTO+","+Utilidades.CAMPO_DIAGNOSTICO+")"
+                + "VALUES ( '"+nombre+"', '"+rut+"', '" +medicamento+"', '"+diagnostico+"' )" ;
+
+        db.execSQL(insert);
+
+        Toast.makeText(getApplicationContext(), "Receta Lista, Imprimiendo...", Toast.LENGTH_SHORT).show();
+
+        db.close(); // MIRA FELIX CULIAO PA QUE VEIA QUE CIERRO LAS CONEXIONES A LAS BASES DE DATOS
+    }
+
+}
